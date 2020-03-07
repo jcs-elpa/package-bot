9a853577d88e20f0708bafedeced8a9fe40786c2
jenchieh <jcs090218@gmail.com>
2020-03-08 00:00:47 +0800

    Add scripts.

---
+++ scripts/run.bat
@@ -0,0 +1,13 @@
+@echo off
+:: ========================================================================
+:: $File: run.bat $
+:: $Date: 2020-03-07 23:56:24 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2020 by Shen, Jen-Chieh $
+:: ========================================================================
+
+cd ..
+
+node index.js

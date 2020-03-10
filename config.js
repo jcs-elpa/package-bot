bb83c644ee3c187a0bed3916281cd4e36f202954
jenchieh <jcs090218@gmail.com>
2020-03-11 00:01:49 +0800

    Update config.

--- config.js
+++ config.js
@@ -9,19 +9,24 @@
 
 "use strict";
 
-const DEBUG = true;
+const DEBUG           = true;
+
+const APP_NAME        = 'package-bot - MELPA';
 
 // Interval time to do PR review.
-const CHECK_TIME = 1000 * 60 * 30;
+const CHECK_TIME      = 1000 * 60 * 30;
 
-const APP_NAME = 'package-bot - MELPA';
+const DATA_PATH       = './status.dat';
+const REVIEW_PATH     = './review/';       // Clone the review package to this path.
+const REVIEW_SCRIPT   = './review.el';
+const OUTPUT_PATH     = './output.txt';    // File that contains review information.
+const REQUEST_DATA    = './request.json';  // Temporary file to output the body form.
 
-const DATA_PATH = './status.dat';
+const TEMPLATE_HEADER = './templates/header.md';
+const TEMPLATE_BODY   = './templates/body.md';
 
-// Clone the review package to this path.
-const REVIEW_PATH = './review/';
-
-const REVIEW_SCRIPT = './review.el';
+const USERNAME        = '';
+const ACCESS_TOKEN    = '';
 
 //------------------- Module Exports -------------------//
 
@@ -31,3 +36,9 @@ module.exports.APP_NAME = APP_NAME;
 module.exports.DATA_PATH = DATA_PATH;
 module.exports.REVIEW_PATH = REVIEW_PATH;
 module.exports.REVIEW_SCRIPT = REVIEW_SCRIPT;
+module.exports.OUTPUT_PATH = OUTPUT_PATH;
+module.exports.REQUEST_DATA = REQUEST_DATA;
+module.exports.TEMPLATE_HEADER = TEMPLATE_HEADER;
+module.exports.TEMPLATE_BODY = TEMPLATE_BODY;
+module.exports.USERNAME = USERNAME;
+module.exports.ACCESS_TOKEN = ACCESS_TOKEN;

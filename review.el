3953e9d1ab27d19bb2c0ea2a726ea0b5f30fd124
jenchieh <jcs090218@gmail.com>
2020-03-11 00:03:56 +0800

    Update review script.

--- review.el
+++ review.el
@@ -12,34 +12,68 @@
 ;; initialize package.el
 (package-initialize)
 
+(require 'f)
+
+(defun review-read-file (path)
+  "Read a file from PATH."
+  (with-temp-buffer
+    (insert-file-contents path)
+    (buffer-string)))
+
+(defun review-write (str &optional append)
+  "Write STR to output file."
+  (write-region str nil output-path append))
+
+(defun review-compile-log ()
+  "Return compile log as string for current buffer."
+  (byte-compile-file (buffer-name))
+  (ignore-errors
+    (with-current-buffer "*Compile-Log*"
+      (buffer-string))))
+
+(defun review-checkdoc ()
+  "Return checkdoc as string for current buffer."
+  (checkdoc)
+  (ignore-errors
+    (with-current-buffer "*Checkdoc Status*"
+      (buffer-string))))
+
+(defun review-package-lint ()
+  "Return package lint as string for current buffer."
+  (package-lint-current-buffer)
+  (ignore-errors
+    (with-current-buffer "*Package-Lint*"
+      (buffer-string))))
 
 (defun review-do ()
   "Do the review package action."
-  (progn  ; byte compile
-    (byte-compile-file (buffer-name))
-    (with-current-buffer "*Compile-Log*"
-      (message "%s" (buffer-string))))
-  (progn  ; checkdoc
-    )
-  (progn  ; package lint
-    (package-lint-current-buffer)
-    (with-current-buffer "*Package-Lint*"
-      (message "%s" (buffer-string)))))
+  (let* ((cmp-log (review-compile-log))
+         (pkg-lnt (review-package-lint))
+         (ckdoc (review-checkdoc))
+         (review-text (format template-str
+                              (buffer-name)
+                              checkdoc-version ckdoc
+                              emacs-version cmp-log
+                              pkg-lnt)))
+    (review-write review-text t)))
 
 
-(unless (boundp 'project-dir)
-  ;; NOTE: Testing only.
-  (setq project-dir "./review/emacs-bazel-mode/"))
+(setq output-path (expand-file-name output-path))
+(setq template-str (review-read-file template-body))
+(setq project-dir (expand-file-name project-dir))
+(review-write "")  ; Clean up.
 
 (message "Checking project directory: %s" project-dir)
 
 (let ((dirs (f-directories project-dir nil t))
       (files-el '()))
+  (push project-dir dirs)
+  (dolist (dir dirs) (add-to-list 'load-path (expand-file-name dir)))
   (dolist (dir dirs)
     (setq files-el (directory-files dir nil "\\.el$"))
     (dolist (file files-el)
-      (setq file (format "%s/%s" dir file))
-      (message "Checking file: %s" file)
+      (setq file (concat dir file))
+      (message "> Checking file: '%s'" file)
       (find-file file)
       (review-do))))
 
